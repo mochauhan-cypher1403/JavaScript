d6408185cb722a5d040d44f85e50bd80c5ab4a46
Mohini Chauhan <mochauhan@microsoft.com>
2025-08-29 09:08:47 +0530

    Adding Singleton objects in creation of objects

--- objects_in_javascript/01_objects.js
+++ objects_in_javascript/01_objects.js
@@ -3,7 +3,9 @@
 WHAT ALL IS COVERED IN THIS FILE 
 1. OBJECTS CREATION - SINGELTON AND OBJECT LITERALS 
 2. ACCESSING OBJECT PROPERTIES 
-
+3. HOW TO DELETE THE PROPERTY OF THE OBJECT
+4. THE USE OF IN OPERATOR FOR OBJECTS 
+5. USING THE "FOR..IN" LOOP TO ACCESS ALL THE PROPERTIES.
 */
 // REFERENCE: https://javascript.info/object
 
@@ -21,10 +23,44 @@ Useful: For things like configuration settings, logging, or managing shared reso
 Important: Not every object is called an instance - some objects are just created directly, not from a class or a constructor.
 When you use a class or an constructor function to create an object, that object is considered an instance of that class.
 */
-
+// ------------------ Singleton object----------------
+// This pattern ensures only one spotifyUser exists and this is shared everywhere in the code.
+const spotifyUserSingleton = (function() {
+    let instance;
+    function createInstance(){
+        return {
+            userId: "563535",
+            userName: "Mohini Chauhan",
+            userAge: 22, 
+            userCountry: "India",
+            userPlaylists: ["Chill Vibes", "Workout Mix", "Top Hits"],
+            userLikedSongs: ["Song1", "Song2", "Song3"],
+            userRecentlyPlayed: ["Song4", "Song5", "Song6"],
+            userFriends: [
+                {
+                    friendId: "123456",
+                    friendName: "John Doe",
+                    friendEmail: "johndoe234@mgmail.com"
+                },
+            ],
+            "isLoggedIn": true, // This cannot be accessed with dot notation, it can only be accessed with bracket notation
+        }
+    }
+    return {
+        getInstance: function() {
+            if (!instance) {
+                instance = createInstance();
+            }
+            return instance;
+        }
+    };
+})();
+const user1 = spotifyUserSingleton.getInstance();
+const user2 = spotifyUserSingleton.getInstance();
+console.log(user1 === user2); // true - both variables point to the same instance
 //-------------------Object Literals----------------------
 let spotifyUser ={};
-Object.create({}); // This will create an empty object // This is the singleton object 
+Object.create({}); // This will create an empty object 
 
 spotifyUser = {
     userId: "6409174",
@@ -80,3 +116,17 @@ Syntax "key" in object
 console.log("user age" in tinderUsers);
 console.log("userAge" in spotifyUser);
 
+// Other way of doing the same thing
+let key = "userPlaylists";
+console.log(key in spotifyUser);
+
+// Why does there is a "in" operator if comparing with undefined is enough?
+/*
+The "in" operator is more explicit and can be used to check for the existence of a property in an object, regardless of its value (even if it's null or undefined).
+This can be particularly useful when you want to differentiate between a property that doesn't exist and one that exists but has an undefined value.
+*/
+
+// for..in loop to access all the properties of an object - properties if non-integer are listed in the creation order.
+for (let key in spotifyUser) {
+    console.log(key + ": " + spotifyUser[key]);
+}
